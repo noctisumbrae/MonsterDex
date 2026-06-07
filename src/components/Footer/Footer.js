7f8e9ff8fa13b9fec4acdd73fcd83fca3d1b9477
“Camila <milagoncc@gmail.com>
2026-06-07 13:07:28 -0300

    Adiciona componentes FilterBar e Footer em src/components

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>
+        Projeto acadêmico sem fins comerciais. Pokémon e seus nomes/personagens
+        pertencem aos seus respectivos titulares. Este projeto não é afiliado,
+        patrocinado ou aprovado pela Nintendo, Game Freak, Creatures ou The
+        Pokémon Company. Dados obtidos via PokéAPI.
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
